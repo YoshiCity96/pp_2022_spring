a0c419a0a5b40f30bb1a56d4c7cead868452dfa6
YoshiCity96 <40843184+YoshiCity96@users.noreply.github.com>
2022-05-24 19:22:46 +0300

    Add files via upload

---
+++ modules/task_1/rudoy_v_sort_doub_simp/discharge_double_sort.h
@@ -0,0 +1,11 @@
+
+#ifndef MODULES_TASK_1_RUDOY_V_SORT_DOUB_SIMP_DISCHARGE_DOUBLE_SORT_H_
+#define MODULES_TASK_1_RUDOY_V_SORT_DOUB_SIMP_DISCHARGE_DOUBLE_SORT_H_
+#include <vector>
+#include <list>
+double* create_random_vector(int size_n);
+void union_from_lists(const std::vector<std::list<double>>* lists, double* in);
+void discharge_sort(double* in, int size);
+bool check_vectors(double* st, double* sd, int size);
+void copy_vectors(double* st, double* sd, int size);
+#endif  // MODULES_TASK_1_RUDOY_V_SORT_DOUB_SIMP_DISCHARGE_DOUBLE_SORT_H_
